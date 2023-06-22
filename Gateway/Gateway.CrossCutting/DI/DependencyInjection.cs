2723a27a3329dfb2f08ad688ef6afbd715a0d52b
Miguel-Bit-Debug <migueldossantos011@gmail.com>
2023-06-21 22:35:36 -0300

    changes crosscutting

--- Gateway/Gateway.CrossCutting/DI/DependencyInjection.cs
+++ Gateway/Gateway.CrossCutting/DI/DependencyInjection.cs
@@ -1,4 +1,12 @@
-﻿using ICut.Domain.Interfaces;
+﻿using FluentValidation;
+using Gateway.Domain.DTOs.Request;
+using Gateway.Domain.ExternalServices;
+using Gateway.Domain.Interfaces.Repositories;
+using Gateway.Domain.Interfaces.Services;
+using Gateway.Domain.Services;
+using Gateway.Domain.Validators;
+using Gateway.InfraData.Data;
+using Gateway.InfraData.Repositories;
 using ICut.Domain.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
@@ -9,7 +17,16 @@ namespace ICut.CrossCutting.DI
     {
         public static void AddDependencyInjection(this WebApplicationBuilder builder)
         {
+            // HTTP
+            builder.Services.AddHttpClient();
+
+            builder.Services.AddTransient<IValidator<AccountRequestDTO>, AccountRequestValidator>();
+            builder.Services.AddScoped(typeof(IEventHubExternalService<>), typeof(EventHubExternalService<>));
+            builder.Services.AddScoped<IAccountService, AccountService>();
+            builder.Services.AddScoped<IAccountRepository, AccountRepository>();
             builder.Services.AddScoped<ITokenService, TokenService>();
+
+            builder.Services.AddSingleton<IMongoDbContext, MongoDbContext>();
         }
     }
 }
